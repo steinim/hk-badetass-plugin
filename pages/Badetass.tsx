7ead1260aa4c1eff31fb7ff8aa5fab8dd680ade2
Stein Inge Morisbak <steinim@gmail.com>
2020-07-28 12:01:44 +0200

    removed unnecessary styling

--- pages/Badetass.tsx
+++ pages/Badetass.tsx
@@ -1,18 +1,9 @@
 import React from 'react';
-import { View, Container } from 'native-base';
+import { View, Container, Text } from 'native-base';
 import BadetassProvider from '../BadetassProvider';
-import { StyleSheet, Platform } from 'react-native';
-import { typography } from 'styles';
 import BadetassLogo from '../assets/BadetassLogo';
 import TemperatureList from '../components/TemperatureList';
 import AreaSelector from '../components/AreaSelector';
-
-const styles = StyleSheet.create({
-  title: {
-    ...typography.textLight,
-    marginBottom: 20,
-  },
-});
 
 export const Badetass = () => {
   return (
